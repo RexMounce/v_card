fca8a8e55a4dcbfc2c1e1a6f8980951e316cb61d
RexMounce <rexmounce@gmail.com>
2025-03-11 22:13:16 -0500

    Add files via upload

--- rexm_v-card.js
+++ rexm_v-card.js
@@ -7,14 +7,39 @@ const profileData = {
   "location": "Springfield, MO",
   "verified_accounts": [
     {
-      "service_label": "WordPress",
-      "service_icon": "https://gravatar.com/icons/wordpress.svg",
-      "url": "https://rexrambler.wordpress.com"
+      "service_label": "Facebook",
+      "service_icon": "https://gravatar.com/icons/facebook.svg",
+      "url": "https://www.facebook.com/rex.rambler"
     },
     {
-      "service_label": "GitHub",
-      "service_icon": "https://gravatar.com/icons/github.svg",
-      "url": "https://github.com/RexMounce"
+      "service_label": "Instagram",
+      "service_icon": "https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png",
+      "url": "https://www.instagram.com/rexrambler/"
+    },
+    {
+      "service_label": "TikTok",
+      "service_icon": "https://upload.wikimedia.org/wikipedia/commons/2/2f/TikTok_Icon.png",
+      "url": "https://www.tiktok.com/@rexmouncerambler"
+    },
+    {
+      "service_label": "YouTube",
+      "service_icon": "https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_social_red_circle_%282017%29.svg",
+      "url": "http://www.youtube.com/@rexmounce"
+    },
+    {
+      "service_label": "Spotify",
+      "service_icon": "https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg",
+      "url": "https://artists.spotify.com/c/artist/4sDyLHN5DtjqfsdTU7lL3J/profile"
+    },
+    {
+      "service_label": "iTunes",
+      "service_icon": "https://upload.wikimedia.org/wikipedia/commons/4/4e/Apple_Music_icon.png",
+      "url": "https://music.apple.com/us/artist/rex-mounce/1512651998"
+    },
+    {
+      "service_label": "DistroKid",
+      "service_icon": "https://distrokid.com/favicon.ico",
+      "url": "https://distrokid.com/hyperfollow/rexmounce/innocent-2"
     }
   ]
 };
